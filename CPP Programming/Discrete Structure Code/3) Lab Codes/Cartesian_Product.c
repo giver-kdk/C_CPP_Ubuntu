0df0ae1bdfc68c5b04707d211726846c43193778
Revig <45360605+giver-kdk@users.noreply.github.com>
2022-03-24 15:44:20 +0545

    Debugging

--- CPP Programming/Discrete Structure Code/3) Lab Codes/Cartesian_Product.c
+++ CPP Programming/Discrete Structure Code/3) Lab Codes/Cartesian_Product.c
@@ -4,30 +4,30 @@
 
 int main()
 {
-	char ch;
-	int a[10], b[10], m, n, i, j;
-	printf("How many elemnets do you want in set a?\n");
-	scanf("%d", &m);
-	printf("Enter %d elements in set a: ", m);
-	for(i = 0;i < m; i++)
-	{
-		scanf("%d", &a[i]);
-	}
-	printf("How many elemnets do you want in set b?\n");
-	scanf("%d", &n);
-	printf("Enter %d elements in set b: ", n);
-	for(j = 0;j < m; j++)
-	{
-		scanf("%d", &b[j]);
-	}
-	printf("\nCartesian Product: ");
-	for(i = 0; i < m; i++)
-	{
-		for(j = 0; j < n; j++)
-		{
-			printf("{%d, %d}, ", a[i], b[j]);
-		}
-	}
-	ch = getchar();
-	return 0;
+    char ch;
+    int a[10], b[10], m, n, i, j;
+    printf("How many elemnets do you want in set a?\n");
+    scanf("%d", &m);
+    printf("Enter %d elements in set a: ", m);
+    for (i = 0; i < m; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("How many elements do you want in set b?\n");
+    scanf("%d", &n);
+    printf("Enter %d elements in set b: ", n);
+    for (j = 0; j < n; j++)
+    {
+        scanf("%d", &b[j]);
+    }
+    printf("\nCartesian Product: ");
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("{%d, %d}, ", a[i], b[j]);
+        }
+    }
+    ch = getchar();
+    return 0;
 }
