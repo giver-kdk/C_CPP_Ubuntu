73652027e42423c6c8e0f5f2bd943635ad32f75e
giver-kdk <giverbro13@gmail.com>
2023-08-10 18:51:40 +0545

    mid square update

--- C Programming/Simulation/Random Number/mid-square.c
+++ C Programming/Simulation/Random Number/mid-square.c
@@ -3,27 +3,27 @@
 
 int main()
 {
-	int seed, square, random = 1, prev, count, n;
+	int temp, seed, square, random = 1, prev, count = 0, n, max = 50;
 	printf("Enter seed value: ");
 	scanf("%d", &seed);
-	while (random != 0 || prev != random){
+	temp = seed;
+	while (temp != 0){
+		temp /= 10;
+		count++;
+	}
+	if (count % 2 == 0) n = count / 2;
+	else n = (count / 2) + 1;
+	
+	while (random != 0){
 		prev = random;
 		random = 1;
-		count = 0;
 		square = seed * seed;
-		while (seed != 0){
-			seed /= 10;
-			count++;
-		}
-		if (count % 2 == 0) n = count / 2;
-		else n = (count / 2) + 1;
-
 		for (int i = 0; i < n; i++)
 			square = square / 10;
 		for (int i = 0; i < count; i++)
 			random = random * 10;
-		random = square % random;
-		if(prev == random) break;
+		random = fabs(square % random);
+		if(prev == random || (max--) == 0) break;
 		printf("%d\n", random);
 		seed = random;
 	}
