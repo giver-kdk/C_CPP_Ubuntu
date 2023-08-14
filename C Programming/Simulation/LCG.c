947cbfff0bb4e38d6e78d4e46a5afddc8cc2eb7e
Revig <giverbro13@gmail.com>
2023-08-14 13:57:16 +0545

    LCG.c
    lcg pseudo code

---
+++ C Programming/Simulation/LCG.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int x0, a, c, m, r;
+    printf("Enter value of x0: ");
+    scanf("%d", &x0);
+    printf("Enter value of a: ");
+    scanf("%d", &a);
+    printf("Enter value of c: ");
+    scanf("%d", &c);
+    printf("Enter value of m: ");
+    scanf("%d", &m);
+
+    x1 = x0;
+    for(int i = 0; i < 20; i++)    
+    {
+        x1 = (a*x1 + c) % m;
+        r = x1 / m;
+        printf("%d", r);
+    }
+}
