099fe9bb5c9a1ae5e344dcdb222ddb47a68322bc
Revig <giverbro13@gmail.com>
2023-08-14 14:12:33 +0545

    Update LCG.c
    updated test

--- C Programming/Simulation/LCG.c
+++ C Programming/Simulation/LCG.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-    int x0, a, c, m, r;
+    int a, c, m, x0, x1;
+    float r;
     printf("Enter value of x0: ");
     scanf("%d", &x0);
     printf("Enter value of a: ");
@@ -17,6 +18,28 @@ int main()
     {
         x1 = (a*x1 + c) % m;
         r = x1 / m;
-        printf("%d", r);
+        printf("%d\t", x1);
+    }
+}#include <stdio.h>
+
+int main()
+{
+    int a, c, m, x0, x1;
+    float r;
+    printf("Enter value of x0: ");
+    scanf("%d", &x0);
+    printf("Enter value of a: ");
+    scanf("%d", &a);
+    printf("Enter value of c: ");
+    scanf("%d", &c);
+    printf("Enter value of m: ");
+    scanf("%d", &m);
+
+    x1 = x0;
+    for(int i = 0; i < 20; i++)    
+    {
+        x1 = (a*x1 + c) % m;
+        r = x1 / m;
+        printf("%d\t", x1);
     }
 }
