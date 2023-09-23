a18962ca06e432c60cfbf64f4c22304f75c2ad7a
giver-kdk <giverbro13@gmail.com>
2023-09-23 11:42:05 +0545

    min max

---
+++ C Programming/DAA/Algorithms/min_max_recursive.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void min_max_algo(int a[], int l, int r, int* min, int* max);
+
+int main()
+{
+    int n = 5, i, min, max;
+    int a[10];
+    printf("Enter %d numbers: \n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    int l = 0, r = n - 1;
+    min_max_algo(a, l, r, &min, &max);
+
+    printf("The min number is %d\n", min);
+    printf("The max number is %d\n", max);
+
+    return 0; 
+}
+
+void min_max_algo(int a[], int l, int r, int* min, int* max)
+{
+    if (l == r)
+    {
+        *min = a[l]; 			
+        *max = a[l]; 			
+    }
+    else if (l == r - 1)
+    {
+        if (a[l] > a[r])
+        {
+            *min = a[r]; 		
+            *max = a[l]; 		
+        }
+        else
+        {
+            *min = a[l]; 		
+            *max = a[r]; 		
+        }
+    }
+    else
+    {
+        int mid = (l + r) / 2;
+        int min1, max1, min2, max2;
+        min_max_algo(a, l, mid, &min1, &max1);
+        min_max_algo(a, mid + 1, r, &min2, &max2);
+
+        *min = (min1 < min2) ? min1 : min2;
+        *max = (max1 > max2) ? max1 : max2;
+    }
+}
